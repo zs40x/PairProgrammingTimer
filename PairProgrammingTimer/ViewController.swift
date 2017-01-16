67211d920710bc88076ca62a6c4bfc0127f15a26
Stefan Mehnert <StefanMehnert@gmail.com>
2017-01-16 20:10:29 +0100

    Added image offset constant outlets and initialization

--- PairProgrammingTimer/ViewController.swift
+++ PairProgrammingTimer/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var leftImageTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var leftImageBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var leftImageLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var rightImageTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var rightImageLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var rightImageBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var rightImageTopConstraint: NSLayoutConstraint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        leftImageTopConstraint.constant = 5
+        leftImageBottomConstraint.constant = 5
+        leftImageLeadingConstraint.constant = 5
+        leftImageTrailingConstraint.constant = 5
+        
+        rightImageTopConstraint.constant = 20
+        rightImageBottomConstraint.constant = 20
+        rightImageLeadingConstraint.constant = 20
+        rightImageTrailingConstraint.constant = 20
+        
     }
 
     override func didReceiveMemoryWarning() {
