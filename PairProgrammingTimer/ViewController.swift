214a2bfae17d29a12e5c4d7990db8c0bb1f905fe
Stefan Mehnert <StefanMehnert@gmail.com>
2017-01-18 05:36:57 +0100

    Removed pause button, resetted hugging priority and compression resistance for the buttons with it's spacer

--- PairProgrammingTimer/ViewController.swift
+++ PairProgrammingTimer/ViewController.swift
@@ -31,7 +31,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var rightImageLeadingConstraint: NSLayoutConstraint!
     @IBOutlet weak var rightImageBottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var rightImageTopConstraint: NSLayoutConstraint!
-    @IBOutlet weak var buttonPause: UIButton!
     @IBOutlet weak var buttonStart: UIButton!
     
     
@@ -45,9 +44,6 @@ class ViewController: UIViewController {
         activeDeveloper = (activeDeveloper == .left ? .right : .left)
         updateImageOffsets()
         updateDeveloperImages()
-    }
-    
-    @IBAction func actionPause(_ sender: Any) {
     }
     
     @IBAction func actionStart(_ sender: Any) {
