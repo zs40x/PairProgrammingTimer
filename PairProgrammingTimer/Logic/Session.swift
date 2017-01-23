70c32c1571e8e4278ecfeceaa64bdd81a1926c9e
Stefan Mehnert <StefanMehnert@gmail.com>
2017-01-23 19:02:57 +0100

    Added session classes

---
+++ PairProgrammingTimer/Logic/Session.swift
@@ -0,0 +1,10 @@
+//
+//  ProgrammingSession.swift
+//  PairProgrammingTimer
+//
+//  Created by Stefan Mehnert on 23/01/2017.
+//  Copyright © 2017 Stefan Mehnert. All rights reserved.
+//
+
+import Foundation
+
---
+++ PairProgrammingTimer/Logic/SessionController.swift
@@ -0,0 +1,9 @@
+//
+//  SessionController.swift
+//  PairProgrammingTimer
+//
+//  Created by Stefan Mehnert on 23/01/2017.
+//  Copyright © 2017 Stefan Mehnert. All rights reserved.
+//
+
+import Foundation
