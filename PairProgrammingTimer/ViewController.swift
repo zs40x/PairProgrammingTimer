df4572d3569412d8405fcc81c65f3360affc7ee5
Stefan Mehnert <StefanMehnert@gmail.com>
2017-02-01 18:45:45 +0100

    Added default interval back to 15 minutes

--- PairProgrammingTimer/ViewController.swift
+++ PairProgrammingTimer/ViewController.swift
@@ -17,7 +17,7 @@ enum Developer: Int {
 
 class ViewController: UIViewController {
     
-    private let countDownMinutes: Double = 0.1
+    private let countDownMinutes: Double = 15
     private let activeOffset: CGFloat = 5
     private let inactiveOffset: CGFloat = 25
     
